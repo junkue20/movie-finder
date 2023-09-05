036010099cd99ee43982d43bb46a381d25d335e7
junkue20 <junkue13@gmail.com>
2023-09-05 17:31:24 +0900

    Add: Serverless Functions

--- src/store/movie.js
+++ src/store/movie.js
@@ -19,32 +19,42 @@ export const searchMovies = async (page) => {
     store.state.message = '';
   }
   try {
-    const res = await fetch(`https://omdbapi.com?apikey=d612176e&s=${store.state.searchText}&page=${page}`);
-  const { Search, totalResults, Response, Error } = await res.json();
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        title: store.state.searchText,
+        page
+      }),
+    });
+    const { Search, totalResults, Response, Error } = await res.json();
 
-  if (Response === 'True') {
-    store.state.movies = [
-      // 배열 리터럴 및 전개 연산자 사용 (1페이지 + 2페이지 정보... 계속해서 누적되는 형식)
-      ...store.state.movies,
-      ...Search,
-    ];
-    store.state.pageMax = Math.ceil(Number(totalResults) / 10);
-  } else {
-    store.state.message = Error;
-    store.state.pageMax = 1;
-  }
+    if (Response === 'True') {
+      store.state.movies = [
+        // 배열 리터럴 및 전개 연산자 사용 (1페이지 + 2페이지 정보... 계속해서 누적되는 형식)
+        ...store.state.movies,
+        ...Search,
+      ];
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10);
+    } else {
+      store.state.message = Error;
+      store.state.pageMax = 1;
+    }
   } catch (error) {
     console.log('searchMovies error: ', error);
   } finally {
     store.state.loading = false;
   }
-
 };
-export const getMovieDetails = async id => {
+export const getMovieDetails = async (id) => {
   try {
-    const res = await fetch(`https://omdbapi.com?apikey=d612176e&i=${id}&plot=full`)  
+    const res = await fetch('/api/movie', {
+      method: 'POST',
+      body: JSON.stringify({
+        id
+      })
+    })
     store.state.movie = await res.json();
   } catch (error) {
-    console.log("getMovieDetails error: ", error);
+    console.log('getMovieDetails error: ', error);
   }
-}
+};
